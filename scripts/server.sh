a6ea4d198a42a6eaf3104463c9fdc2601cdcf1c1
Kevin DeLoach <kdeloach@azavea.com>
2017-01-31 12:21:40 -0500

    Use docker-compose up instead of run
    This fixes an issue where the RWD service ports are not mapped when the
    server command is running, which results in the following error:
    
    ```
    > curl http://localhost:5000
    curl: (7) Failed to connect to localhost port 5000: Connection refused
    ```

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,14 +4,6 @@ set -e
 
 echo "Run the RWD Docker container."
 
-if [ -z "$RWD_DATA" ]; then
-    echo "Environment variable RWD_DATA not defined."
-    exit 1
-fi
-
 set -x
 
-docker-compose run \
-    --rm \
-    -e RWD_DATA=$RWD_DATA \
-    app
+docker-compose up
